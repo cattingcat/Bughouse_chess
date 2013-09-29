cc8b59e2653481de7f2ae2a93480a06c35284c61
zxc123zxc <mark.martynov@yahoo.com>
2013-09-29 15:30:10 +0400

    add selecting cell

--- bughouse_chess/chessboard.h
+++ bughouse_chess/chessboard.h
@@ -15,7 +15,6 @@ public:
         BLACK = 8,
         WHITE = 16
     };
-
     enum Chessman{
         CHESSMAN_KING = 0,
         CHESSMAN_QUEEN = 1,
@@ -28,9 +27,9 @@ public:
 private:
     uint **field;
     uint pad;
+    QPoint *selected_cell;
 
 public:
-
     Chessboard(QWidget* parent = 0): QWidget(parent){
         field = new uint*[8];
         for(int i = 0; i < 8; ++i){
@@ -40,21 +39,29 @@ public:
         pad = 25;
     }
 
+    virtual ~Chessboard(){
+        for(int i = 0; i < 8; ++i){
+            delete field[i];
+        }
+        delete field;
+    }
+
+protected:
     virtual void paintEvent(QPaintEvent *e){
-        QPainter p(this);
+        QPainter painter(this);
         int min_sz = minDim();
 
         QRect widget_rect = e->rect();
-        p.drawRect(widget_rect);
-        p.drawRect(0, 0, min_sz, min_sz);
+        painter.drawRect(widget_rect);
+        painter.drawRect(0, 0, min_sz, min_sz);
 
         min_sz -= pad;
         int d = min_sz / 8;
         for(int i = 0; i < min_sz; i += d){
-            p.drawLine(i + pad, 0, i + pad, min_sz);
-            p.drawLine(pad, i, min_sz + pad, i);
+            painter.drawLine(i + pad, 0, i + pad, min_sz);
+            painter.drawLine(pad, i, min_sz + pad, i);
         }
-        p.drawLine(pad, min_sz, min_sz + pad, min_sz);        
+        painter.drawLine(pad, min_sz, min_sz + pad, min_sz);
 
 
         for(int i = 0; i < 8; ++i){
@@ -63,14 +70,37 @@ public:
                 QRect cr = getRectByCoord(i, j);
                 bool flag2 = j % 2 == 0;
                 if(flag && flag2  || (!flag) && (!flag2)){
-                    p.fillRect(cr, Qt::blue);
+                    painter.fillRect(cr, Qt::blue);
                 }
             }
         }
 
-        QRect cell = getRectByCoord(3, 5);
-        p.drawPixmap(cell, QPixmap(":/img/w_king.png"));
+        if(selected_cell){
+            QRect cell = getRectByCoord(*selected_cell);
+            qDebug() << cell;
+            painter.drawPixmap(cell, QPixmap(":/img/w_king.png"));
+        }
 
+    }
+
+    virtual void mousePressEvent(QMouseEvent *e){
+        int x = e->x();
+        int y = e->y();
+        int min_sz = minDim();
+        if(x > min_sz || y > min_sz)
+            return;
+        int d = (min_sz - pad) / 8;
+        int x_cell = (x - pad) / d;
+        int y_cell = 7 - (y / d);
+        if(selected_cell)
+            delete selected_cell;
+        selected_cell = new QPoint(x_cell, y_cell);
+        qDebug() << *selected_cell;
+        repaint();
+    }
+
+    QRect getRectByCoord(QPoint p){
+        return getRectByCoord(p.x(), p.y());
     }
 
     QRect getRectByCoord(int x, int y){
@@ -91,6 +121,10 @@ public:
         int tmp = dim / 8.0;
         return tmp * 8 + pad;
     }
+
+signals:
+    void cellClicked(int x, int y);
+
 };
 
 #endif // CHESSBOARD_H
